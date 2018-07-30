3f3f3f3254554bdfc53ebb830a7eeb1b6471aa73
sxcsch <sxcsch@gmail.com>
2018-07-30 17:02:22 +0800

    正常运行，更改开发模式。增加yml文件支持。druid监控控制

--- springbootcommon/src/main/java/cn/eddy/config/DruidConfiguration.java
+++ springbootcommon/src/main/java/cn/eddy/config/DruidConfiguration.java
@@ -3,8 +3,12 @@ package cn.eddy.config;
 import com.alibaba.druid.filter.Filter;
 import com.alibaba.druid.filter.stat.StatFilter;
 import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -15,30 +19,35 @@ import java.util.Arrays;
  * Created by Administrator on 2018/5/8.
  */
 @Configuration
-/*@PropertySource("classpath:application.properties")*/
 public class DruidConfiguration {
-   /* @Value("${spring.datasource.druid.driver-class-name}")
-    private String driver;
-    @Value("${spring.datasource.druid.url}")
-    private String url;
-    @Value("${spring.datasource.druid.username}")
-    private String username;
-    @Value("${spring.datasource.druid.password}")
-    private String password;
-    @Value("${spring.datasource.druid.max-active}")
-    private String maxActive;*/
 
     @ConfigurationProperties(prefix = "spring.datasource.druid")
     @Bean(initMethod = "init",destroyMethod = "close")
     public DruidDataSource dataSource() {
         DruidDataSource ds = new DruidDataSource();
-       /* ds.setProxyFilters(Arrays.asList(statFilter()));
-        ds.setDriverClassName(driver);
-        ds.setUrl(url);
-        ds.setUsername(username);
-        ds.setPassword(password);
-        ds.setMaxActive(Integer.parseInt(maxActive));*/
         return ds;
+    }
+
+    /**
+     * 注册一个StatViewServlet
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean druidStatViewServlet(){
+        //org.springframework.boot.context.embedded.ServletRegistrationBean提供类的进行注册.
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet(),"/druid/*");
+
+        //添加初始化参数：initParams
+        //白名单：
+        servletRegistrationBean.addInitParameter("allow","127.0.0.1");
+        //IP黑名单 (存在共同时，deny优先于allow) : 如果满足deny的话提示:Sorry, you are not permitted to view this page.
+        //servletRegistrationBean.addInitParameter("deny","192.168.1.73");
+        //登录查看信息的账号密码.
+        servletRegistrationBean.addInitParameter("loginUsername","admin");
+        servletRegistrationBean.addInitParameter("loginPassword","admin");
+        //是否能够重置数据.
+        servletRegistrationBean.addInitParameter("resetEnable","false");
+        return servletRegistrationBean;
     }
 
     @Bean
@@ -48,5 +57,22 @@ public class DruidConfiguration {
         filter.setLogSlowSql(true);
         filter.setMergeSql(true);
         return filter;
+    }
+
+    /**
+     * 注册一个：filterRegistrationBean
+     * @return
+     */
+    @Bean
+    public FilterRegistrationBean druidStatFilter(){
+
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new WebStatFilter());
+
+        //添加过滤规则.
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        //添加不需要忽略的格式信息.
+        filterRegistrationBean.addInitParameter("exclusions","*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        return filterRegistrationBean;
     }
 }
