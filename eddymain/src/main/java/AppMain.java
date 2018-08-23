6fa6159d809304bec74ca01bcf67658aa80acf61
sxcsch <sxcsch@gmail.com>
2018-08-23 18:06:12 +0800

    提交文件

--- eddymain/src/main/java/AppMain.java
+++ eddymain/src/main/java/AppMain.java
@@ -4,6 +4,11 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 /**
  * Created by Administrator on 2018/7/30.
@@ -15,8 +20,99 @@ public class AppMain {
             System.out.println(new String(bytes,"gbk"));*/
 
             List list = new ArrayList<String>();
+            AppMain t = new AppMain();
+            List<Future<String>> futureList = new ArrayList<Future<String>>();
+            t.generate(3, futureList);
+            t.doOtherThings();
+            t.getResult(futureList);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+    /**
+     * 生成指定数量的线程，都放入future数组
+     *
+     * @param threadNum
+     * @param fList
+     */
+    public void generate(int threadNum, List<Future<String>> fList) {
+        ExecutorService service = Executors.newFixedThreadPool(threadNum);
+        for (int i = 0; i < threadNum; i++) {
+            Future<String> f = service.submit(getJob(i));
+            fList.add(f);
+        }
+        service.shutdown();
+    }
+
+    /**
+     * other things
+     */
+    public void doOtherThings() {
+        try {
+            for (int i = 0; i < 3; i++) {
+                System.out.println("do thing no:" + i);
+                Thread.sleep(1000 * (new Random().nextInt(10)));
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 从future中获取线程结果，打印结果
+     *
+     * @param fList
+     */
+    public void getResult(List<Future<String>> fList) {
+        ExecutorService service = Executors.newSingleThreadExecutor();
+        service.execute(getCollectJob(fList));
+        service.shutdown();
+    }
+
+    /**
+     * 生成指定序号的线程对象
+     *
+     * @param i
+     * @return
+     */
+    public Callable<String> getJob(final int i) {
+        final int time = new Random().nextInt(10);
+        return new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                Thread.sleep(1000 * time);
+                return "thread-" + i;
+            }
+        };
+    }
+
+    /**
+     * 生成结果收集线程对象
+     *
+     * @param fList
+     * @return
+     */
+    public Runnable getCollectJob(final List<Future<String>> fList) {
+        return new Runnable() {
+            public void run() {
+                for (Future<String> future : fList) {
+                    try {
+                        while (true) {
+                            if (future.isDone() && !future.isCancelled()) {
+                                System.out.println("Future:" + future
+                                        + ",Result:" + future.get());
+                                break;
+                            } else {
+                                Thread.sleep(1000);
+                            }
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+    }
+
 }
